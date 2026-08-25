e84cca0e577990cd807da4903a62f7db3d8ece4a
dtenwolde <dljtw98@gmail.com>
2026-08-25 08:58:59 +0200

    Upgrade usearch to 2.26.0

---
+++ src/include/usearch/README.md
@@ -0,0 +1,2 @@
+Source: https://github.com/unum-cloud/USearch/releases#release-v2.26.0
+Tag: 2.26.0
--- src/include/usearch/duckdb_usearch.hpp
+++ src/include/usearch/duckdb_usearch.hpp
@@ -1,16 +1,14 @@
 #pragma once
 
-#include <unordered_map>
 #include <cfloat>
+#include <unordered_map>
 
-#define USEARCH_USE_SIMSIMD DUCKDB_USEARCH_USE_SIMSIMD
-#define USEARCH_USE_FP16LIB 1
+#define USEARCH_USE_NUMKONG 0
 #define USEARCH_USE_OPENMP  0
 
 #include "usearch/index.hpp"
 #include "usearch/index_dense.hpp"
 #include "usearch/index_plugins.hpp"
 
-#undef USEARCH_USE_SIMSIMD
-#undef USEARCH_USE_FP16LIB
+#undef USEARCH_USE_NUMKONG
 #undef USEARCH_USE_OPENMP
